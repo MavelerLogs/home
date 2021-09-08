461689ab528e520a492297ec08836cca83d21485
Prasanna kondreddy <76090299+prasannakondreddy@users.noreply.github.com>
2021-09-08 13:33:40 +0530

    Landing page added

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+$('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '10px',
+          slidesToShow: 1
+        }
+      
+      
+      }
+    ]
+  })
+
+ 
